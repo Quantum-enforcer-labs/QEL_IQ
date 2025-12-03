de3c6188a2efa8ca5bf6415a1b39c37776e240d0
Prosper Chitewe <Prosperchitewe691@gmail.com>
2025-12-03 17:57:02 +0200

    setting up the database

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -6,4 +6,10 @@ export const ENV = {
   PORT: process.env.PORT,
   MONGO_URL: process.env.MONGO_URL,
   NODE_ENV: process.env.NODE_ENV,
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_SECRET_KEY: process.env.STREAM_SECRET_KEY,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 };
