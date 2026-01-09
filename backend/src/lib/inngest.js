4719be963cf058d469d0fe7294c79b5d97f66d11
Prosper Chitewe <Prosperchitewe691@gmail.com>
2026-01-09 13:04:39 +0200

    auth and db sync

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,43 @@
+import { Inngest, inngest } from "inngest";
+import { connectDatabase } from "./db.js";
+import { User } from "../models/User.js";
+
+export const inngest = new Inngest({ id: "qel-iq" });
+
+const syncUser = inngest.createFunction(
+  {
+    id: "sync-user",
+  },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDatabase();
+
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      profileImage: image_url,
+    };
+
+    await User.create(newUser);
+  }
+);
+
+const deleteUserFromDb = inngest.createFunction(
+  {
+    id: "delete-user-from-db",
+  },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDatabase();
+
+    const { id } = event.data;
+
+    await User.findOneAndDelete({ clerkId: id });
+  }
+);
+
+export const functions = [syncUser, deleteUserFromDb];
